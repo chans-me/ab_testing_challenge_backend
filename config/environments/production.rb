930a1ce5d3d814d0ea32bbd9bc43fb594a0b0c03
Admin Nourishdaa <admin@nourishdaa.in>
2021-06-03 22:43:13 +0530

    Added cors fix to frontend call

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,12 +88,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-       origins '*'
-       resource '*', :headers => :any, :methods => [:get, :post, :options]
-     end
-  end
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
